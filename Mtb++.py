843c72959daa928b9000a3b004055e24efca7da4
MSEali <m.serajian@ufl.edu>
2023-09-20 22:06:24 -0400

    Relative directories checked!

--- Mtb++.py
+++ Mtb++.py
@@ -9,9 +9,15 @@ import string
 import subprocess
 import csv
 import sys
-# Set the working directory to the directory containing your main script
-print(sys.argv[0])
-os.chdir(os.path.dirname(__file__))
+# Get the current directory of the script
+current_dir = os.path.dirname(os.path.abspath(__file__))
+
+# Get the parent directory (project root) of the current directory
+project_root = os.path.dirname(current_dir)
+
+# Add the project root directory to PYTHONPATH
+sys.path.append(project_root)
+
 # Global varible, drug names:
 
 # Drug_number, orders 
@@ -44,13 +50,14 @@ def main():
     
     args = parser.parse_args()
 
+
     # Check if the output file path is an absolute path or a relative path
     if not os.path.isabs(args.o):
         # Construct the absolute path for the output file based on the current working directory
         output_file_address = os.path.abspath(args.o)
     else:
         output_file_address = args.o
-    
+
     
     input_file_address=args.f
 
@@ -67,19 +74,38 @@ def main():
     #the Ascii code and doing the classification 
     #at the same time
     
+
+    #These lists contain the information that will further be reported
+
     header= [" "]+drug_names
     LR_report=["Logistic Regression"]
     RF_report=["Random Forest"]
     drug_number=0
+
     for drug in drug_names:
+
+        #Address to the SBWK index for each specific drug
+        SBWT_index = os.path.join(current_dir, "data", "SBWT_indexes","{}.sbwt".format(drug))
+
+        #Temporary random file names that will be removed later on
+        #temporary_file=current_dir+"/temp/"+prefix_temporary_files+"_"+drug+".txt"
+        temporary_file = os.path.join(current_dir, "temp", prefix_temporary_files+"_"+drug+".txt")
+
+        #Runing the SBWK Kmer Counter to create the color matrix
+
+        #Address to the SBWK Kmer Counter executable
         address_to_kmer_counters_executable= "."+ os.path.join('/src',"SBWT-kmer-counters","counters") 
-        SBWT_index="/home/m.serajian/projects/MTB-plus-plus/data/SBWT_indexes/{}.sbwt".format(drug)
-        temporary_file="temp/"+prefix_temporary_files+"_"+drug+".txt"
+
         command= address_to_kmer_counters_executable+ " "+ SBWT_index + " " + input_file_address+">"+temporary_file
+        #Runing the command
         subprocess.run(command, check=False, shell=True,stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        
+        #Prediciting AMR by pretrained models
         prediction=resistance_predictor.AMR_predictor(temporary_file,drug_number)
-        LR_report.append(prediction[0])
-        RF_report.append(prediction[1])
+
+        #Saving to the lists
+        LR_report.append(prediction[0]) #LR
+        RF_report.append(prediction[1]) #RF
         os.remove(temporary_file)
         drug_number=drug_number+1
     
@@ -91,6 +117,14 @@ def main():
         csv_writer.writerow(header)
         csv_writer.writerow(LR_report)
         csv_writer.writerow(RF_report)
+        
+    # ANSI escape code for blue color
+    blue_color = "\033[34m"
+    reset_color = "\033[0m"  # Reset color to default
+
+    # Print the message with the output_file_address in blue
+    print("The AMR predictions are stored at {}{}{} directory!".format(blue_color, output_file_address, reset_color))
+
 
 
 if __name__ == "__main__":
