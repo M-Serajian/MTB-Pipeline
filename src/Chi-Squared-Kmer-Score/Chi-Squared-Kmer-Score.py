950384400a34380613ce794ee0f554f077898192
Mohammadali (Ali) Serajian <97544341+M-Serajian@users.noreply.github.com>
2024-03-25 17:27:54 -0400

    Variables Checked

--- src/Chi-Squared-Kmer-Score/Chi-Squared-Kmer-Score.py
+++ src/Chi-Squared-Kmer-Score/Chi-Squared-Kmer-Score.py
@@ -6,35 +6,7 @@ import pandas as pd
 import sys
 import copy
 
-
-
-import argparse
-parser = argparse.ArgumentParser(description="Process arguments")
-parser.add_argument('-1', dest='arg1', type=str, required=True, help="file_number")
-parser.add_argument('-2', dest='arg2', type=str, required=True, help="Drug name")
-parser.add_argument('-3', dest='arg3', type=str, required=True, help="Number of samples (fasta files)")
-parser.add_argument('-4', dest='arg4', type=str, required=True, help="Address to the Kmers stored")
-parser.add_argument('-5', dest='arg5', type=str, required=True, help="Desierd directory for the outputs")
-parser.add_argument('-6', dest='arg6', type=str, required=True, help="Address to the index of cross-validation")
-parser.add_argument('-7', dest='arg7', type=str, required=True, help="The address to the phenotypes")
-parser.add_argument('-8', dest='arg8', type=str, required=True, help="Number of cross-validation folds (5 in our study!)")
-parser.add_argument('-9', dest='arg9', type=str, required=True, help="Which fold of cross-validation (which fold to be validation and the rest are training) (from zero to 4 for 5 fold cross-validation)")
-args = parser.parse_args()
-
-
-i=str(args.arg1) #file number, we considered 12 files
-drug_name_group=args.arg2
-Number_of_samples=int(args.arg3)
-Kmers_address=args.arg4
-Chi_score_addresses_for_each_drug=args.arg5
-Cross_validation_indexes_address=args.arg6
-The_address_to_phenotypes=args.arg7
-Cross_validation_folds=args.arg8
-Cross_validation_index=args.arg9
-
-df=pd.read_csv(The_address_to_phenotypes)
-# Separating test and train data
-# Chi-Score should only be performed on the train data
+# This function is defined to just calculate the Chi-score test on the 
 
 def get_non_nan_indices(array):
     non_nan_indices = []
@@ -43,78 +15,64 @@ def get_non_nan_indices(array):
             non_nan_indices.append(i)
     return non_nan_indices
 
+# Parsing arguments
+file_number=str(sys.argv[1]) 
+drug_name_group=sys.argv[2] # abbreviation
+Kmers_address=sys.argv[3] # address to the .npy files
+Chi_score_addresses_for_each_drug=sys.argv[4] # where to save the chiscores
+CV_fold=sys.argv[5] #number (0-4)
+train_index_address=sys.argv[6] # Cross validation address
+labels_address=sys.argv[7] # phenotypes
 
-#--------------------------------------------------#
-list_indexes=[]
-#Loading the index of each fold into the list_indexes list
-for i in range (Cross_validation_folds):
-   list_indexes.append(np.load(\
-      Cross_validation_indexes_address+\
-      "indexes_of_fold_{}.npy".format(i)))
-
-#Choosing the Cross_validation_index in the list_indexes as validation or test set
-test_index =list_indexes[Cross_validation_index]
-
-list_indexes.pop[Cross_validation_index]
-
-#considering the rest of folds in index_list as training set
-train_index= np.concatenate(list_indexes)
-
-
-# Loading Targets
-print("Columns in the CSV file are:")
-print(df.columns,flush=True)
-
-
-# Which drug? See below
-drug_names=['ERR', 'ID', 'ERS',"Amikacin", "Bedaquiline", \
-            "Clofazimine","Delamanid","Ethambutol", "Ethionamide",\
-            "Isoniazid","Kanamycin","Levofloxacin","Linezolid",\
-            "Moxifloxacin", "Rifampicin","Rifabutin",\
-            "RIA","AMG","FQS"]
+print(f"The drug is: {drug_name_group}")
 
 
 
+train_index=np.load(train_index_address)
+
+# processing the targets
+df=pd.read_csv(labels_address)
+
+# The columns in the dataframe:
+drug_names= ["ERR","ENA_RUN","AMI",\
+             "BDQ","CFZ","DLM","EMB",\
+             "ETH","INH","KAN","LEV",\
+             "LZD","MXF","RIF","RFB",\
+             "RIA","AMG","FQS"]
 
 try:
   CSV_index = drug_names.index(drug_name_group)
 except Exception as error:
   raise ValueError("Could not find the drug name; \n \
                    The drug names are: \n \
-                   Amikacin ,\n \
-                   Bedaquiline ,\n \
-                   Clofazimine,\n \
-                   Delamanid ,\n \
-                   Ethambutol ,\n \
-                   Ethionamide ,\n \
-                   Isoniazid ,\n \
-                   Kanamycin ,\n \
-                   Levofloxacin ,\n \
-                   Linezolid ,\n \
-                   Moxifloxacin ,\n \
-                   Rifampicin ,\n \
-                   Rifabutin ,\n \
-                   RIA ,\n \
-                   AMG ,\n \
-                   FQS ")
+                   AMI, \n \
+                   BDQ,\n \
+                   CFZ,\n \
+                   DLM,\n \
+                   EMB,\n \
+                   ETH,\n \
+                   INH,\n \
+                   KAN,\n \
+                   LEV,\n \
+                   LZD,\n \
+                   MXF,\n \
+                   RIF,\n \
+                   RFB,\n \
+                   RIA,\n \
+                   AMG,\n \
+                   FQS")
 
-print("Drug Found!",flush=True)
 
-# Columns of the target file are: 
-#IDX:0      1     2      3      4      5      6
-# ['ERR', 'ID', 'ERS', 'AMI', 'BDQ', 'CFZ', 'DLM',
-# 'EMB', 'ETH', 'INH','KAN', 'LEV', 'LZD', 'MXF',
-# 'RIF', 'RFB', 'RIFBB', 'Ami_Kan','Fluoroquinolone']
-   
+
 
 group=CSV_index 
 print("Column on CSV is {}".format(group),flush=True)
 drug_name=drug_names[group]
 target=np.array(df)
 
+
 phenotype=target[:,group]
 phenotype=phenotype[train_index] # Separating the train set
-
 non_ambiguous_indecies=get_non_nan_indices(phenotype)
 
 
@@ -130,37 +88,24 @@ print(np.shape(phenotype),flush=True)
 
 
 
-
-# Base address for the data (all the Kmers generated by SBWT)
-base_address=Kmers_address
-
-
-
-file_name="counts_file"+i+".npy"
+# Reading the data
+data=np.load(f"{Kmers_address}{file_number}.npy", allow_pickle=True)
+data=np.stack(data, axis=0)
 
 
-print(drug_name+" is being checked",flush=True)
 
-print("---------------------------- file {} ---------------------------------------".format(i),flush=True)
-
-data=np.load(base_address+file_name)
-print("The total data shape is {}".format(np.shape(data)),flush=True)
-#important Kmers are the rows and samples are the columns
-
-
-print("Train data size with ambiguous phenotypes is: {}\n".format(np.shape(train_index)),flush=True)
-
-
-print("Train data size without ambiguous phenotypes is: {}\n".format(np.shape(non_ambiguous_train_indecies)),flush=True)
-
-
+print(data[0])
+print(np.shape(data))
 
 train_data=data[:,non_ambiguous_train_indecies] # The color matrix generated by SBWT (rows are kmers and columns are strands)
 
-counter=0
-chi_score=[] #list of chi score
+print(train_data[0])
+print(np.shape(train_data))
 
-# Chi-square test
+counter=0
+chi_score=[]
+
+
 for j in range (np.size(train_data,0)): # Kmers are in the rows
     row=train_data[j,:]  
     contigency_pct = pd.crosstab(row,phenotype)
@@ -169,10 +114,12 @@ for j in range (np.size(train_data,0)): # Kmers are in the rows
 
 chi_score=np.array(chi_score)
 
-np.save(Chi_score_addresses_for_each_drug +drug_name+\
-        "/chi_score_file"+i+"_fold_number_{}.npy".format(Cross_validation_index),chi_score)
+np.save(Chi_score_addresses_for_each_drug+f"chi_square_{file_number}.npy",chi_score)
 
-print("The output chi square array's size is : {}".\
-      format(np.shape(chi_score)))
+print(chi_score)
+counter=0
+for i in chi_score: 
+   if i>4 :
+    counter= counter+1
 
-print("----------------------------  Finished  ----------------------------")
+print("The number of times chiscore is higher than 4 is: {}".format(counter))
